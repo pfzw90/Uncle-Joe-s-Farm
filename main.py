d2013b308154f3326136290cd2d1452d55c1fa45
Natalia Chekhonina <pfzw90@gmail.com>
2020-09-23 08:26:28 +0000

    added weighting functions

--- main.py
+++ main.py
@@ -86,12 +86,24 @@ def create_farm():
   uncle_joe_farm.append(Cow('Манька', 350))
   uncle_joe_farm.append(Duck('Кряква', 5))
 
-  def find_animal_type(sound = input('Input animal sound:')):
+  def find_animal_type():
+    sound = input('Input animal sound: ')
     for animal in uncle_joe_farm:
       if animal.sound == sound:
         print(type(animal).__name__)
         return
 
-  find_animal_type();
+  def find_haviest_animal():
+    print(f'The haviest animal is {sorted(uncle_joe_farm, key=lambda x: x.weight, reverse = True)[0].name}.')
+
+  def find_total_weight():
+    total_weight = 0
+    for animal in uncle_joe_farm:
+      total_weight += animal.weight
+    print(f'Total weight of all the animals in the farm: {total_weight} kgs.')
+
+  find_haviest_animal()
+  find_total_weight()
+  find_animal_type()
 
 create_farm()
